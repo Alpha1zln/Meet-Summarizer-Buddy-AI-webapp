a89f4cfb6e2ec6a612bc38f4035155edfccbb329
SHREYAS YADUVANSHI <98889077+Alpha1zln@users.noreply.github.com>
2024-11-09 16:01:36 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -42,53 +42,6 @@ function verifyTextLength(e) {
 
 
 
-
-// function submitData(e) {
-
-//   // This is used to add animation to the submit button
-//   submitButton.classList.add("submit-button--loading");
-
-//   const text_to_summarize = textArea.value;
-
-//   // INSERT CODE SNIPPET FROM POSTMAN BELOW
-//   // This is the function where the call to the API is made. Returns the summarized text as a string.
-
-//   const axios = require('axios');
-//   let data = JSON.stringify({
-//     "text_to_summarize": "Amelia, a curious astronomer, spotted a peculiar comet through her telescope. It emitted a mesmerizing glow and seemed to change course. Determined to investigate, she built a spaceship. On her journey, she encountered cosmic wonders, yet the comet remained elusive. Finally, at the edge of the universe, she understood its secret: the comet was a celestial guide, leading her to discover the boundless wonders within herself. Grateful for the voyage, Amelia returned, sharing her newfound wisdom with the world, inspiring others to explore both the cosmos and the depths of their souls."
-//   });
-
-//   let config = {
-//     method: 'post',
-//     maxBodyLength: Infinity,
-//     url: 'PASTE YOUR REPLIT WEBVIEW URL HERE AND IN CURRENT VALUE COLUMN/summarize',
-//     headers: { 
-//       'Content-Type': 'application/json', 
-//       'Authorization': 'Bearer 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'
-//     },
-//     data : data
-//   };
-
-//   async function makeRequest() {
-//     try {
-//       const response = await axios.request(config);
-//       console.log(JSON.stringify(response.data));
-//     }
-//     catch (error) {
-//       console.log(error);
-//     }
-//   }
-
-//   makeRequest();
-
-
-
-  
-// }
-
-
-
-
 function submitData(e) {
 
  // This is used to add animation to the submit button
